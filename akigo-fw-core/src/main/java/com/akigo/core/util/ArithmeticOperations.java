1e51d7121ed4f5d12475e276010d2800446b2a4f
システムズ_陳浩 <kou-chin@h.jfe-systems.com>
2019-07-08 19:20:13 +0900

    ArithmeticOperations init ver

---
+++ akigo-fw-core/src/main/java/com/akigo/core/util/ArithmeticOperations.java
@@ -0,0 +1,219 @@
+package com.akigo.core.util;
+
+import com.akigo.core.exception.SystemException;
+import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ArithmeticOperations {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ArithmeticOperations.class);
+
+    @FunctionalInterface
+    private interface CalcStrategy extends Serializable {
+        void calc(Stack<BigDecimal> numStack, Stack<Operator> opStack);
+    }
+
+    private enum Operator {
+        /**
+         * 足算
+         */
+        PLUS("+", 1, (numStack, opStack) -> {
+            BigDecimal o2 = numStack.pop();
+            BigDecimal o1 = numStack.pop();
+
+            BigDecimal result = o1.add(o2);
+
+            LOGGER.debug(o1.toPlainString() + " + " + o2.toPlainString() + " = " + result);
+
+            numStack.push(result);
+        }),
+        /**
+         * 引算
+         */
+        SUB("-", 1, (numStack, opStack) -> {
+            BigDecimal o2 = numStack.pop();
+            BigDecimal o1 = numStack.pop();
+
+            BigDecimal result = o1.subtract(o2);
+
+            LOGGER.debug(o1.toPlainString() + " - " + o2.toPlainString() + " = " + result);
+
+            numStack.push(result);
+        }),
+        /**
+         * 掛算
+         */
+        MULTI("*", 2, (numStack, opStack) -> {
+            BigDecimal o2 = numStack.pop();
+            BigDecimal o1 = numStack.pop();
+
+            BigDecimal result = o1.multiply(o2);
+
+            LOGGER.debug(o1.toPlainString() + " * " + o2.toPlainString() + " = " + result);
+
+            numStack.push(result);
+        }),
+        /**
+         * 割算
+         */
+        DIV("/", 2, (numStack, opStack) -> {
+            BigDecimal o2 = numStack.pop();
+            BigDecimal o1 = numStack.pop();
+
+            int scale = o1.scale() > o2.scale() ? o1.scale() : o2.scale();
+
+            BigDecimal result = o1.divide(o2, scale, RoundingMode.HALF_DOWN);
+
+            LOGGER.debug(
+                    o1.toPlainString() + " / " + o2.toPlainString() + " = " + result + " scale:" + scale);
+
+            numStack.push(result);
+        }),
+        /**
+         * 左括弧
+         */
+        LEFT_PARENTHESIS("(", 3, (numStack, opStack) -> {
+            // 何もしない
+        }),
+        /**
+         * 右括弧
+         */
+        RIGHT_PARENTHESIS(")", 3, (numStack, opStack) -> {
+            Operator lastOp;
+            while ((lastOp = opStack.pop()) != LEFT_PARENTHESIS) {
+                lastOp.calc(numStack, opStack);
+            }
+        });
+
+        @Getter
+        private String operator;
+        @Getter
+        private int priority;
+        private CalcStrategy calcFunc;
+
+        public static Optional<Operator> of(String operator) {
+            return Arrays.stream(Operator.values()).filter(op -> op.getOperator().equals(operator))
+                    .findFirst();
+        }
+
+        private Operator(String operator, int priority,
+                         CalcStrategy calcFunc) {
+            this.operator = operator;
+            this.priority = priority;
+            this.calcFunc = calcFunc;
+        }
+
+        public void calc(Stack<BigDecimal> numStack, Stack<Operator> opStack) {
+            try {
+                this.calcFunc.calc(numStack, opStack);
+            } catch (Exception e) {
+                throw new SystemException("ArithmeticOperations evaluation is failed.", e);
+            }
+        }
+    }
+
+    /**
+     * 計算式文字列解析処理<br>
+     *
+     * <pre>
+     * 例：
+     * 1 + 2 * ( -3 + 4 )の解析結果は、
+     *
+     * [1, +, 2, *, (, -3, +, 4, )]
+     * </pre>
+     *
+     * @param calcExpression 計算式文字列
+     * @return 解析後のすべて計算式元素のリスト
+     */
+    public List<String> parseCalcExpression(String calcExpression) {
+
+        LOGGER.debug("before parse:{}", calcExpression);
+
+        List<String> calcElements = new ArrayList<>();
+        List<String> opList = Arrays.stream(Operator.values()).map(Operator::getOperator).collect(
+                Collectors.toList());
+
+        String parseStr = calcExpression.trim();
+        int idxPointer = 0;
+        for (int i = 0; i < parseStr.length(); i++) {
+            String tempStr = String.valueOf(parseStr.charAt(i));
+            if (opList.contains(tempStr)) {
+                if (!Operator.SUB.getOperator().equals(tempStr) ||
+                        (Operator.SUB.getOperator().equals(tempStr) &&
+                                i < (parseStr.length() - 1) &&
+                                " ".equals(String.valueOf(parseStr.charAt(i + 1))))) {
+                    // "-"の直後は半角スペースのある場合は、引算と看做されて、半角スペースのない場合は、マイナス数字と看做される
+                    if (i != 0) {
+                        String subStr = parseStr.substring(idxPointer, i).trim();
+                        if (!subStr.isEmpty()) {
+                            calcElements.add(subStr);
+                        }
+                    }
+                    calcElements.add(tempStr);
+                    idxPointer = i + 1;
+                }
+            }
+        }
+        if (idxPointer < parseStr.length()) {
+            calcElements.add(parseStr.substring(idxPointer, parseStr.length()).trim());
+        }
+
+        LOGGER.debug("after parse:{}", calcElements);
+
+        return calcElements;
+    }
+
+    /**
+     * 計算式実行処理<br>
+     *
+     * @param calcExpression
+     * @return 計算式実行結果
+     */
+    public BigDecimal eval(String calcExpression) {
+        // 計算式文字列解析
+        List<String> elements = parseCalcExpression(calcExpression);
+
+        Stack<BigDecimal> numStack = new Stack<>();
+        Stack<Operator> opStack = new Stack<>();
+
+        for (String ele : elements) {
+            Optional<Operator> op = Operator.of(ele);
+            if (op.isPresent()) {
+                if (op.get() == Operator.RIGHT_PARENTHESIS) {
+                    op.get().calc(numStack, opStack);
+                } else {
+                    while (!opStack.isEmpty() &&
+                            op.get().getPriority() <= opStack.peek().getPriority() &&
+                            opStack.peek() != Operator.LEFT_PARENTHESIS) {
+                        Operator peekOp = opStack.pop();
+                        peekOp.calc(numStack, opStack);
+                    }
+                    opStack.push(op.get());
+                }
+            } else {
+                try {
+                    numStack.push(new BigDecimal(ele));
+                } catch (NumberFormatException e) {
+                    throw new SystemException(String.format("[%s] is not a number.", ele), e);
+                }
+            }
+        }
+
+        while (!opStack.isEmpty()) {
+            opStack.pop().calc(numStack, opStack);
+        }
+
+        BigDecimal result = numStack.peek();
+
+        LOGGER.debug("eval result:{}", result.toPlainString());
+
+        return result;
+    }
+}
---
+++ akigo-fw-core/src/test/java/com/akigo/core/util/ArithmeticOperationsTest.java
@@ -0,0 +1,45 @@
+package com.akigo.core.util;
+
+import com.akigo.core.exception.SystemException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class ArithmeticOperationsTest {
+
+    @Test
+    public void evalTest01() {
+        ArithmeticOperations calculator = new ArithmeticOperations();
+        BigDecimal result = calculator.eval(
+                "(3 + 4 * 2.4 / 2.123 + ( (-1 - (5)) * (2 + 100) + 1.12345 / 2.22 ))");
+    }
+
+    @Test
+    public void evalTest02() {
+        ArithmeticOperations calculator = new ArithmeticOperations();
+        try {
+            BigDecimal result = calculator.eval(
+                    "(3 + A * 2.4 / 2.123 + ( (-1 - (5)) * (2 + 100) + 1.12345 / 2.22 ))");
+        } catch (SystemException e) {
+            System.out.println(e);
+            Assert.assertTrue(true);
+            return;
+        }
+        Assert.fail();
+    }
+
+    @Test
+    public void evalTest03() {
+        ArithmeticOperations calculator = new ArithmeticOperations();
+        try {
+            BigDecimal result = calculator.eval(
+                    "(3 +- 4 * 2.4 / 2.123 + ( (-1 - (5)) * (2 + 100) + 1.12345 / 2.22 ))");
+        } catch (SystemException e) {
+            System.out.println(e);
+            Assert.assertTrue(true);
+            return;
+        }
+        Assert.fail();
+    }
+}
